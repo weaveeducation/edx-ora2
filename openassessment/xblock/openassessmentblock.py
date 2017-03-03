a67213b1897f7fd38ff296a859bdf6cfd5f8b2eb
Dmitry Viskov <dmitry.viskov@webenterprise.ru>
2017-03-03 15:26:36 +0300

    EPM-184 - New xblock aside for ora items

--- openassessment/xblock/openassessmentblock.py
+++ openassessment/xblock/openassessmentblock.py
@@ -486,7 +486,30 @@ class OpenAssessmentBlock(MessageMixin,
 
         return self._create_fragment(template, context_dict, initialize_js_func='StaffAssessmentBlock')
 
-    def _create_fragment(self, template, context_dict, initialize_js_func, additional_css=None, additional_js=None):
+    def author_view(self, context=None):
+        # should be similar as student_view
+
+        try:
+            self.update_workflow_status()
+        except AssessmentWorkflowError:
+            # Log the exception, but continue loading the page
+            logger.exception('An error occurred while updating the workflow on page load.')
+
+        ui_models = self._create_ui_models()
+        # All data we intend to pass to the front end.
+        context_dict = {
+            "title": self.title,
+            "prompts": self.prompts,
+            "rubric_assessments": ui_models,
+            "show_staff_area": self.is_course_staff and not self.in_studio_preview,
+        }
+        template = get_template("openassessmentblock/oa_base.html")
+
+        return self._create_fragment(template, context_dict, initialize_js_func='OpenAssessmentBlock',
+                                     min_postfix='author-view')
+
+    def _create_fragment(self, template, context_dict, initialize_js_func,
+                         additional_css=None, additional_js=None, min_postfix=''):
         """
         Creates a fragment for display.
 
@@ -522,7 +545,10 @@ class OpenAssessmentBlock(MessageMixin,
             fragment.add_css(load(css_url))
 
             # minified additional_js should be already included in 'make javascript'
-            fragment.add_javascript(load("static/js/openassessment-lms.min.js"))
+            if min_postfix:
+                fragment.add_javascript(load("static/js/openassessment-lms-%s.min.js" % min_postfix))
+            else:
+                fragment.add_javascript(load("static/js/openassessment-lms.min.js"))
         js_context_dict = {
             "ALLOWED_IMAGE_MIME_TYPES": self.ALLOWED_IMAGE_MIME_TYPES,
             "ALLOWED_FILE_MIME_TYPES": self.ALLOWED_FILE_MIME_TYPES,
@@ -531,6 +557,17 @@ class OpenAssessmentBlock(MessageMixin,
         }
         fragment.initialize_js(initialize_js_func, js_context_dict)
         return fragment
+
+    def author_view(self, context=None):
+        return self.student_view(context)
+
+    @property
+    def has_author_view(self):
+        return True
+
+    @property
+    def has_author_view(self):
+        return True
 
     @property
     def is_admin(self):
