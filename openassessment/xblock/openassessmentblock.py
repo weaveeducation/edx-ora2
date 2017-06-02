879adeef494345813e8cad42e42ddccb2b21ad56
Dmitry Viskov <dmitry.viskov@webenterprise.ru>
2017-06-02 13:06:43 +0300

    Add prompts to the events

--- openassessment/xblock/openassessmentblock.py
+++ openassessment/xblock/openassessmentblock.py
@@ -1093,7 +1093,8 @@ class OpenAssessmentBlock(MessageMixin,
             "score_type": assessment["score_type"],
             "scored_at": assessment["scored_at"],
             "submission_uuid": assessment["submission_uuid"],
-            "parts": parts_list
+            "parts": parts_list,
+            "prompts": self.prompts
         }
 
         for key in kwargs:
